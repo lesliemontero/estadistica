90864063a2b88be6a0d35f7d9584919c5cef01f0
lesliemontero <143569816+lesliemontero@users.noreply.github.com>
2024-01-15 22:52:01 -0600

    Gráfica de barras 4 proyecto
    Gráfica de barras 4 proyecto. Equipo: Leslie, Laura, Aranza

---
+++ Gráfica de barras 4 proyecto final.R
@@ -0,0 +1,27 @@
+attach(G4)
+
+
+media_reactivos <- colMeans(G4[,c("Bajo",
+                                  "Deficiente",
+                                  "Regular",
+                                  "Bueno",
+                                  "Sobresaliente")])
+
+media_reactivos
+
+barplot(media_reactivos,
+        main = "Gráfico de barras",
+        ylim = c(0,5),
+        xlab = "Maestros encuestados",
+        ylab = "Escala",
+        col = c("darkslategray"))
+
+
+
+barplot(media_reactivos, main = "Gráfico de barras",
+        ylim = c(0,10),
+        xlim = c(0,6),
+        ylab = "maestros encuestados",
+        xlab = "Escala",
+        col = c("cyan3"),
+        width = c(0.8))
